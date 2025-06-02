3234187dacd48b7475b42720b6f537eee458cf00
qaws7791 <qaws7791@gmail.com>
2025-06-02 17:16:59 +0900

    style: prettier 설정 변경
    notion에 있는 prettier 설정 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 /// <reference types="vitest" />
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
     coverage: {
-      provider: "v8",
+      provider: 'v8',
     },
-    environment: "jsdom",
-    include: ["./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    setupFiles: ["./vitest-setup.js"],
+    environment: 'jsdom',
+    include: ['./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    setupFiles: ['./vitest-setup.js'],
   },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
