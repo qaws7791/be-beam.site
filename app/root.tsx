958f8cdd56c12cdad5c9e760120f23abec27a8ea
qaws7791 <qaws7791@gmail.com>
2025-06-08 21:17:48 +0900

    feat: pretendard 폰트 추가

--- app/root.tsx
+++ app/root.tsx
@@ -11,15 +11,11 @@ import type { Route } from './+types/root';
 import './app.css';
 
 export const links: Route.LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
   {
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+    as: 'style',
+    crossOrigin: 'anonymous',
+    href: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css',
   },
 ];
 
