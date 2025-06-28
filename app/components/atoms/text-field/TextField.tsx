2923d1e2ba3d8d496a036d31e36442508094b510
qaws7791 <qaws7791@gmail.com>
2025-06-28 23:56:58 +0900

    fix: TextField 클리어 기능 수정
    TextField에서 처리하지 않고 rightSection을 통해 외부에서 추가하도록 변경

--- app/components/atoms/text-field/TextField.tsx
+++ app/components/atoms/text-field/TextField.tsx
@@ -1,102 +1,56 @@
 import * as React from 'react';
-import XIcon from '../icons/XIcon';
 import { Label } from '../label/Label';
 import { FormMessage } from '../form/FormMessage';
 import { Input } from '../input/Input';
 import { cn } from '@/lib/tailwind';
+import InputDeleteIcon from '../icons/InputDeleteIcon';
 
 interface TextFieldProps extends React.ComponentProps<'input'> {
   isClearable?: boolean;
   label: string;
   description?: string;
   error?: string;
+  rightSection?: React.ReactNode;
 }
 
 export const TextField = React.forwardRef<
   React.ComponentRef<'input'>,
   TextFieldProps
->(
-  (
-    {
-      isClearable = false,
-      className,
-      defaultValue,
-      onChange,
-      label,
-      description,
-      error,
-      ...props
-    },
-    ref,
-  ) => {
-    const [internalValue, setInternalValue] = React.useState(
-      defaultValue || '',
-    );
-    const isControlled = props.value !== undefined;
-
-    const displayValue = isControlled ? props.value : internalValue;
-    const innerRef = React.useRef<HTMLInputElement>(null);
-
-    const setRefs = (node: HTMLInputElement) => {
-      innerRef.current = node;
-      if (typeof ref === 'function') {
-        ref(node);
-      } else if (ref) {
-        ref.current = node;
-      }
-    };
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (isControlled) {
-        onChange?.(e);
-      } else {
-        setInternalValue(e.target.value);
-      }
-    };
-
-    const handleClear = () => {
-      const syntheticEvent = {
-        target: { ...innerRef.current, value: '' } as HTMLInputElement,
-        currentTarget: { ...innerRef.current, value: '' } as HTMLInputElement,
-        // 필요에 따라 다른 이벤트 속성을 추가할 수 있습니다.
-      } as React.ChangeEvent<HTMLInputElement>;
-      if (!isControlled) {
-        setInternalValue('');
-      }
-      onChange?.(syntheticEvent);
-      innerRef.current?.focus();
-    };
-
-    return (
-      <div className={cn('relative flex flex-col gap-2', className)}>
-        <Label>{label}</Label>
-        <div className="relative">
-          <Input
-            ref={setRefs}
-            value={displayValue}
-            onChange={handleInputChange}
-            aria-invalid={!!error}
-            {...props}
-          />
-          {isClearable && displayValue && (
-            <button
-              onClick={handleClear}
-              className={'absolute top-1/2 right-3 -translate-y-1/2'}
-              type="button"
-              aria-label="Clear"
-            >
-              <XIcon className="text-gray-500" />
-            </button>
-          )}
-        </div>
-        {error ? (
-          <FormMessage variant="error">{error}</FormMessage>
-        ) : description ? (
-          <FormMessage>{description}</FormMessage>
-        ) : null}
+>(({ className, label, description, error, rightSection, ...props }, ref) => {
+  return (
+    <div className={cn('relative flex flex-col gap-2', className)}>
+      <Label>{label}</Label>
+      <div className="relative">
+        <Input ref={ref} aria-invalid={!!error} {...props} />
+        {rightSection && (
+          <div className="absolute top-1/2 right-2 -translate-y-1/2">
+            {rightSection}
+          </div>
+        )}
       </div>
-    );
-  },
-);
+      {error ? (
+        <FormMessage variant="error">{error}</FormMessage>
+      ) : description ? (
+        <FormMessage>{description}</FormMessage>
+      ) : null}
+    </div>
+  );
+});
+
+export const ClearButton = (props: React.ComponentProps<'button'>) => {
+  return (
+    <button
+      className={cn(
+        'flex cursor-pointer items-center justify-center p-1',
+        props.className,
+      )}
+      type="button"
+      aria-label="Clear"
+      {...props}
+    >
+      <InputDeleteIcon className="size-6 text-gray-500" />
+    </button>
+  );
+};
 
 TextField.displayName = 'TextField';
