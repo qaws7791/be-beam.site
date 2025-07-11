941bb900fabbc708f9b2c156420fb05baf363451
qaws7791 <qaws7791@gmail.com>
2025-07-11 17:34:11 +0900

    feat: 알림 popover 추가

--- app/components/molecules/NavbarUserSection.tsx
+++ app/components/molecules/NavbarUserSection.tsx
@@ -1,3 +1,11 @@
+import useMyNotificationsQuery from '@/hooks/api/useMyNotificationsQuery';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '../atoms/popover/Popover';
+import CloseIcon from '../atoms/icons/CloseIcon';
+
 export default function NavbarUserSection({
   profileImage,
   onClick,
@@ -6,13 +14,18 @@ export default function NavbarUserSection({
   onClick: () => void;
 }) {
   return (
-    <div className="flex items-center gap-x-4">
+    <div className="flex items-center justify-center gap-x-4">
       <button className="cursor-pointer">
         <img src="/images/icons/like.svg" alt="like_icon" />
       </button>
-      <button className="cursor-pointer">
-        <img src="/images/icons/bell.svg" alt="bell_icon" />
-      </button>
+      <Popover>
+        <PopoverTrigger className="cursor-pointer">
+          <img src="/images/icons/bell.svg" alt="bell_icon" />
+        </PopoverTrigger>
+        <PopoverContent className="z-[100] w-[400px] rounded-xl">
+          <NotificationPopover />
+        </PopoverContent>
+      </Popover>
       <button onClick={onClick}>
         <img
           className="h-7 w-7 cursor-pointer rounded-full object-cover"
@@ -20,6 +33,35 @@ export default function NavbarUserSection({
           alt="profileImg"
         />
       </button>
+    </div>
+  );
+}
+
+function NotificationPopover() {
+  const myNotifications = useMyNotificationsQuery();
+  return (
+    <div>
+      <div>
+        <p className="text-t3 text-gray-800">알림</p>
+      </div>
+      <ul className="mt-5 flex max-h-[400px] flex-col gap-3 overflow-y-auto">
+        {myNotifications.data?.alarms.map((alarm) => (
+          <li
+            key={alarm.id}
+            className="flex items-start justify-between rounded-lg border border-gray-300 px-5 py-4"
+          >
+            <div className="flex h-full flex-col justify-between">
+              <p className="line-clamp-2 h-10 text-b2 text-gray-800">
+                {alarm.message}
+              </p>
+              <time className="text-c3 text-gray-500">{alarm.timestamp}</time>
+            </div>
+            <button aria-label="알림 삭제" type="button">
+              <CloseIcon className="size-6 text-gray-700" />
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
