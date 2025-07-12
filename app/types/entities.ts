4b8350241b405a6cfc7fe4ef3990de1919ae2d4d
qaws7791 <qaws7791@gmail.com>
2025-07-12 22:52:01 +0900

    feat: 엔티티 타입 추가

---
+++ app/types/entities.ts
@@ -0,0 +1,150 @@
+export type MeetingRecruitmentType = '정기모임' | '소모임';
+export type MeetingRecruitmentStatus =
+  | '모집예정'
+  | '모집중'
+  | '모집종료'
+  | '모임중'
+  | '모임완료';
+
+export interface Guidebook {
+  id: number;
+  guidebookType: string;
+  title: string;
+  description: string;
+  images: string[];
+  hashtags: string[];
+  level: string;
+  targetType: string;
+  time: string;
+  benefits: string[];
+  file: string;
+  recommendations: GuidebookRecommendation[];
+}
+
+export interface GuidebookRecommendation {
+  id: number;
+  title: string;
+  image: string;
+}
+
+export interface Topic {
+  id: number;
+  topic: string;
+}
+
+export interface MeetingSchedule {
+  meetingStartTime: string;
+  meetingEndTime: string;
+  address: string;
+  addressDetail: string;
+  content: string;
+}
+
+export interface Meeting {
+  id: number;
+  recruitingState: MeetingRecruitmentStatus;
+  recruitmentType: MeetingRecruitmentType;
+  guidebookReferenceId: number | null;
+  guidebookReferenceTitle: string | null;
+  selectionType: string;
+  name: string;
+  topic: string;
+  participantCount: number;
+  minParticipants: number;
+  maxParticipants: number;
+  recruitingStartTime: string;
+  recruitingEndTime: string;
+  meetingMode: string;
+  address: string;
+  paymentAmount: number;
+  introduction: string;
+  schedules: MeetingSchedule[];
+  hashtags: string[];
+  meetingImages: string[];
+  info: string;
+  hostId: number;
+  hostName: string;
+  hostImage: string;
+  hostDescription: string;
+  likesCount: number;
+  /** 로그인한 사용자 정보 */
+  liked: boolean;
+  reviewable: boolean;
+  userStatus: string;
+}
+
+export interface Host {
+  hostName: string;
+  hostImage: string;
+  hostInstruction: string;
+  followCount: number;
+  openingMeetingCount: number;
+  openingMeetings: {
+    id: number;
+    meetingName: string;
+    meetingImage: string;
+    topic: string;
+  }[];
+  /** 로그인한 사용자 정보 */
+  followed: boolean;
+}
+
+export interface UserInformation {
+  name: string;
+  phoneNumber: string;
+  email: string;
+  birthday: string;
+  gender: '여성' | '남성';
+  /** 약관 동의 여부 */
+  terms: boolean;
+  userTerms: boolean;
+  marketingTerms: boolean;
+}
+
+export interface UserProfile {
+  nickname: string;
+  introduction: string;
+  profileImage: string;
+  role: string;
+}
+
+export interface MeetingSummary {
+  id: number;
+  title: string;
+  recruitmentType: string;
+  image: string;
+  meetingStartTime: string;
+  address: string;
+  status: string;
+}
+
+export interface Review {
+  reviewId: number;
+  profileImg: string;
+  nickname: string;
+  rating: number;
+  text: string;
+  images: string[];
+  createdAt: string;
+  likesCount: number;
+  /** 로그인한 사용자 정보 */
+  liked: boolean;
+  myReview: boolean;
+}
+
+export interface Banner {
+  bannerId: number;
+  bannerImg: string;
+  bannerUrl: string;
+}
+
+export type NotificationType = 'MEETING' | 'REVIEW' | 'HOST';
+
+export interface Notification {
+  id: number;
+  type: NotificationType;
+  message: string;
+  timestamp: string;
+  redirectUrl: string;
+  isRead: boolean;
+}
