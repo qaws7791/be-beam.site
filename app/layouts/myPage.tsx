40f9117ef132a08c7c641b2adc905c64d980ba02
qaws7791 <qaws7791@gmail.com>
2025-07-23 10:56:40 +0900

    feat: 마이페이지 경로 보호

--- app/layouts/myPage.tsx
+++ app/layouts/myPage.tsx
@@ -3,6 +3,9 @@ import { Outlet } from 'react-router';
 import MyPageTemplate from '@/components/templates/MyPageTemplate';
 import SideBar from '@/components/organisms/SideBar';
 import useUserSession from '@/hooks/business/useUserSession';
+import { requireAuthMiddleware } from '@/middlewares/auth';
+
+export const unstable_middleware = [requireAuthMiddleware];
 
 export default function MyPage() {
   const { user } = useUserSession();
