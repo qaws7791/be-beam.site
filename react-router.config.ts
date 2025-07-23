a629636c696ecaafc1732eb1fd9c8889a241cfb2
qaws7791 <qaws7791@gmail.com>
2025-07-23 10:54:51 +0900

    feat: 세션 업데이트 및 인증을 위한 middleware 추가

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,18 @@
 import type { Config } from '@react-router/dev/config';
 import { vercelPreset } from '@vercel/react-router/vite';
 
+declare module 'react-router' {
+  interface Future {
+    unstable_middleware: true;
+  }
+}
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
   presets: [vercelPreset()],
+  future: {
+    unstable_middleware: true,
+  },
 } satisfies Config;
