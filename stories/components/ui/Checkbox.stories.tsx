39da05e4559043317c0fd6e5ee906560fb33e158
qaws7791 <qaws7791@gmail.com>
2025-08-03 04:30:30 +0900

    feat: Checkbox 스토리 추가

---
+++ stories/components/ui/Checkbox.stories.tsx
@@ -0,0 +1,49 @@
+import { Checkbox } from '@/components/atoms/checkbox/Checkbox';
+import { Label } from '@/components/atoms/label/Label';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+const meta = {
+  title: 'Components/UI/Checkbox',
+  component: Checkbox,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+    },
+    checked: {
+      control: 'boolean',
+    },
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Checked: Story = {
+  args: {
+    checked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const WithLabel: Story = {
+  render: (args) => (
+    <div className="flex items-center gap-2">
+      <Checkbox {...args} id="checkbox-with-label" />
+      <Label htmlFor="checkbox-with-label">Checkbox with label</Label>
+    </div>
+  ),
+  args: {},
+};
