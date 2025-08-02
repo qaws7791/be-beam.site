d1dc7403b65d335a31fbfd98054dc38f2db4451c
qaws7791 <qaws7791@gmail.com>
2025-08-02 23:48:41 +0900

    feat: api 최신화

--- app/api/users.ts
+++ app/api/users.ts
@@ -3,6 +3,7 @@ import { axiosInstance } from '@/lib/axios';
 import type { APIResponse } from '@/types/api';
 import type {
   Host,
+  ImageType,
   LinkType,
   Meeting,
   MeetingSchedule,
@@ -46,18 +47,23 @@ export type MyMeetingLikesParams = {
 };
 
 export type MyMeetingLikesResult = {
-  nickname: UserProfile['nickname'];
-  profileImage: UserProfile['profileImage'];
   meetings: {
     id: Meeting['id'];
-    title: Meeting['name'];
+    name: Meeting['name'];
+    recruitmentStatus: Meeting['recruitmentStatus'];
     recruitmentType: Meeting['recruitmentType'];
-    image: Meeting['meetingImages'];
+    thumbnailImage: ImageType;
     meetingStartTime: MeetingSchedule['meetingStartTime'];
     address: Meeting['address'];
-    status: Meeting['userStatus'];
     liked: Meeting['liked'];
   }[];
+  pageInfo: {
+    page: number;
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    hasNext: boolean;
+  };
 };
 
 export const getMyMeetingLikes = async ({
@@ -65,89 +71,13 @@ export const getMyMeetingLikes = async ({
   size,
   type,
 }: MyMeetingLikesParams) => {
-  return {
-    nickname: '홍길동',
-    profileImage: 'https://placehold.co/300',
-    meetings: [
-      {
-        id: 1,
-        title: '모임1',
-        recruitmentType: '정기모임',
-        image: 'https://placehold.co/600x400',
-        meetingStartTime: '2024-10-15T15:00:00',
-        address: '부산 동래구 복천동',
-        status: '모집종료',
-        liked: true,
-      },
-      {
-        id: 2,
-        title: '소셜다이닝 : 이상식탁',
-        recruitmentType: '소모임',
-        image: 'https://placehold.co/600x400',
-        meetingStartTime: '2024-10-15T15:00:00',
-        address: '부산 동래구 복천동',
-        status: '모집종료',
-        liked: true,
-      },
-      {
-        id: 3,
-        title: '모임3',
-        recruitmentType: '정기모임',
-        image: 'https://placehold.co/600x400',
-        meetingStartTime: '2024-10-16T15:00:00',
-        address: '부산 동래구 복천동',
-        status: '모집중',
-        liked: false,
-      },
-      {
-        id: 4,
-        title: '소셜다이닝 : 이상식탁2',
-        recruitmentType: '소모임',
-        image: 'https://placehold.co/600x400',
-        meetingStartTime: '2024-10-16T15:00:00',
-        address: '부산 동래구 복천동',
-        status: '모집중',
-        liked: false,
-      },
-      {
-        id: 5,
-        title: '소셜다이닝 : 이상식탁3',
-        recruitmentType: '소모임',
-        image: 'https://placehold.co/600x400',
-        meetingStartTime: '2024-10-16T15:00:00',
-        address: '부산 동래구 복천동',
-        status: '모집중',
-        liked: false,
-      },
-      {
-        id: 6,
-        title: '소셜다이닝 : 이상식탁4',
-        recruitmentType: '소모임',
-        image: 'https://placehold.co/600x400',
-        meetingStartTime: '2024-10-16T15:00:00',
-        address: '부산 동래구 복천동',
-        status: '모집중',
-        liked: false,
-      },
-      {
-        id: 7,
-        title: '소셜다이닝 : 이상식탁5',
-        recruitmentType: '소모임',
-        image: 'https://placehold.co/600x400',
-        meetingStartTime: '2024-10-16T15:00:00',
-        address: '부산 동래구 복천동',
-        status: '모집중',
-        liked: false,
-      },
-    ],
-  };
   const searchParams = new URLSearchParams({
     cursor: page.toString(),
     size: size.toString(),
     type,
   });
   const res = await axiosInstance.get<APIResponse<MyMeetingLikesResult>>(
-    `users/likes/meetings?${searchParams.toString()}`,
+    `users/meetings/likes?${searchParams.toString()}`,
   );
   const data = res.data;
   return data.result;
